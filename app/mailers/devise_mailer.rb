6dd6d03029c6819260520fa4f07b7d0c7552f85d
Kojo <kodeon6@gmail.com>
2025-10-15 09:35:03 +0100

    Configure devise-passwordless with correct mailer class
    - Update Devise mailer to use Devise::Passwordless::Mailer
    - Update custom DeviseMailer to inherit from passwordless mailer
    - Remove custom magic_link method as it's provided by the gem

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,14 +1,4 @@
-class DeviseMailer < ApplicationMailer
-  def magic_link(record, token, opts = {})
-    @token = token
-    @resource = record
-    @scope_name = Devise::Mapping.find_scope!(record)
-    @remember_me = opts && opts[:remember_me] || false
-
-    template_path = "devise/mailer"
-    mail(to: record.email, subject: "Your magic link - ProdFinder", template_path: template_path)
-  end
-
+class DeviseMailer < Devise::Passwordless::Mailer
   def confirmation_instructions(record, token, opts = {})
     @token = token
     @resource = record
